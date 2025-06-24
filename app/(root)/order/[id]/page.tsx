fc83ff38212ffde926d8d380f23a27e49bb1eefd
anthonypaton <anthonypaton1@sky.com>
2025-06-24 20:03:23 +0100

    still issues with params and a promise

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -1,5 +1,5 @@
-import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
 import { getOrderById } from "@/lib/actions/order.actions";
 import { shippingAddress } from "@/types";
 import OrderDetailsTable from "./order-details-table";
@@ -8,11 +8,15 @@ export const metadata: Metadata = {
   title: "Order Details",
 };
 
-const OrderDetailsPage = async ({ params }: { params: { id: string } }) => {
+interface Params {
+  params: { id: string };
+}
+
+const OrderDetailsPage = async ({ params }: Params) => {
   const { id } = params;
+
   const rawOrder = await getOrderById(id);
   if (!rawOrder) notFound();
-
   const order = {
     ...rawOrder,
     User: rawOrder.user,
@@ -23,26 +27,28 @@ const OrderDetailsPage = async ({ params }: { params: { id: string } }) => {
   };
 
   return (
-    <OrderDetailsTable
-      order={{
-        ...order,
-        itemsPrice: Number(order.itemsPrice),
-        shippingPrice: Number(order.shippingPrice),
-        taxPrice: Number(order.taxPrice),
-        totalPrice: Number(order.totalPrice),
-        shippingAddress: order.shippingAddress as shippingAddress,
-        User: order.User,
-        deliveredAt: order.deliveredAt,
-        paidAt: order.paidAt,
-        isDelivered: order.isDelivered,
-        isPaid: order.isPaid,
-        orderItems: order.orderItems,
-        id: order.id,
-        createdAt: order.createdAt,
-        paymentMethod: order.paymentMethod,
-      }}
-      paypalClientId={process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || "sb"}
-    />
+    <div>
+      <OrderDetailsTable
+        order={{
+          ...order,
+          itemsPrice: Number(order.itemsPrice),
+          shippingPrice: Number(order.shippingPrice),
+          taxPrice: Number(order.taxPrice),
+          totalPrice: Number(order.totalPrice),
+          shippingAddress: order.shippingAddress as shippingAddress,
+          User: order.User,
+          deliveredAt: order.deliveredAt,
+          paidAt: order.paidAt,
+          isDelivered: order.isDelivered,
+          isPaid: order.isPaid,
+          orderItems: order.orderItems,
+          id: order.id,
+          createdAt: order.createdAt,
+          paymentMethod: order.paymentMethod,
+        }}
+        paypalClientId={process.env.PAYPAL_CLIENT_ID || "sb"}
+      />
+    </div>
   );
 };
 
